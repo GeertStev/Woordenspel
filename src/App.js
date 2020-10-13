c1f8c39c03a3c3d5d458e4200519d01b26239881
Geert <geert.stev@gmail.com>
2020-10-13 10:12:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,56 +2,68 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+const klinkers = ["a","aa","e","ee","o","oo","i","ui"];
+const medeklinkers = ["m","n","l","s","t","k","p"];
+
+
+function genereerWoord() {
+  const medeklinker_voor = medeklinkers[~~(Math.random() * medeklinkers.length)];
+  const klinker_midden = klinkers[~~(Math.random() * klinkers.length)];  
+  const medeklinker_achter = medeklinkers[~~(Math.random() * medeklinkers.length)];
+  
+  return([medeklinker_voor+klinker_midden+medeklinker_achter, medeklinker_voor, klinker_midden+medeklinker_achter, klinker_midden,medeklinker_achter] )
+} 
+
+
+class Woord extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.woord = genereerWoord();
+    this.state = {splitsingTonen: 0,
+                  woordTonen: this.woord[0]};
+    
+    this.splitsClick = this.splitsClick.bind(this);
+    this.volgendeClick = this.volgendeClick.bind(this);
+  }
+  
+  splitsClick() {
+    this.setState(prevState => ({
+      splitsingTonen: prevState.splitsingTonen + 1
+    }));
+  }
+  
+  volgendeClick() {
+    this.woord = genereerWoord();
+    this.setState(prevState => ({
+      splitsingTonen: 0
+    }));
+  }
+  
+  
+  render() {
+   
+    return(
+      <div>
+      <div
+        className="left"
+        onClick={this.splitsClick}>
+        <h1>&#128075;</h1>
+      </div>
+      <div
+        className="main">
+        <h1>{this.woord[0]}</h1>
+        <h1>{this.state.splitsingTonen>=1 ? this.woord[1]+" "+this.woord[2] : ""}</h1>
+        <h1>{this.state.splitsingTonen>=2 ? this.woord[3]+" "+this.woord[4] : ""}</h1>
+      </div>
+      <div
+        className="right"
+        onClick={this.volgendeClick}>
+        <h1>&#128077;</h1>
+      </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Woord;
